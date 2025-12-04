0815647f93222df17588e5034119215d21346239
TheVigil <jwschindler@gmaill.com>
2025-12-04 14:00:16 +0100

    added a toy component and a dev cert

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = "Dating App";
+  protected members = signal<any>([]);
+  
+  async ngOnInit() {
+    this.members.set(await this.getMembers()) 
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+    } catch (error) {
+      console.log(error) 
+      throw error
+    }
+  }
 }
