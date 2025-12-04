011be0e1122355cab44fbbe0260a3d8d3ff18e39
TheVigil <jwschindler@gmaill.com>
2025-12-04 15:16:44 +0100

    added basic user reg

---
+++ backend/API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+    {
+        if (await EmailExists(registerDTO.Email))
+        {
+            return BadRequest("Email taken");
+        }
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            DisplayName = registerDTO.DisplayName,
+            Email = registerDTO.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+
+}
