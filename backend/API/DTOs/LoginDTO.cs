bbc2ea54b071ec451e1922cf138e267dbe1e27d2
TheVigil <jwschindler@gmaill.com>
2025-12-05 17:57:44 +0100

    added simplistic login

---
+++ backend/API/DTOs/LoginDTO.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDTO
+{
+    public string Email { get; set;} = "";
+    public string Password { get; set; } = "";
+
+}
