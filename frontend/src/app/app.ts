e4e8bdcdafc24ff97ce4a71986752cd5d0023c6c
TheVigil <jwschindler@gmaill.com>
2026-04-21 11:28:02 +0200

    added a custom toast service

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, Inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { lastValueFrom, throwError } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, Home],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -19,6 +18,7 @@ export class App implements OnInit{
   private accountService = inject(AccountService);
   protected readonly title = "Dating App";
   protected members = signal<User[]>([]);
+  protected router = inject(Router)
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
