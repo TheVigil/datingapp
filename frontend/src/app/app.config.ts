88020200dd8da2572a0a6be76cb62ee353c025fc
TheVigil <jwschindler@gmaill.com>
2026-04-23 13:49:38 +0200

    pushing current state

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { InitService } from '../core/services/init-service';
@@ -8,7 +8,7 @@ import { lastValueFrom } from 'rxjs';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, Inject, OnInit, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom, throwError } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -13,32 +9,6 @@ import { User } from '../types/user';
   styleUrl: './app.css'
 })
 
-export class App implements OnInit{
-  private http = inject(HttpClient);
-  private accountService = inject(AccountService);
-  protected readonly title = "Dating App";
-  protected members = signal<User[]>([]);
+export class App {
   protected router = inject(Router)
-
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser(); 
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-    this.accountService.isAuthReady.set(true);
-  }
-
-  async getMembers(){
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'))
-    } catch (error) {
-      console.log(error) 
-      throw error
-    }
-  }
 }
